8680dd96b5c3b0142d4ec919de4b79260e3b2772
tamishhossain-svg <tamishhossain@gmail.com>
2026-03-15 08:16:48 +0600

    Add files via upload

---
+++ space invaders.py
@@ -0,0 +1,93 @@
+import pygame
+import random
+
+from pygame.examples.video import backgrounds
+
+# initialize pygame
+pygame.init()
+
+# create screen
+screen = pygame.display.set_mode((800,600))
+
+# Background
+background = pygame.image.load('space.png')
+
+
+
+# Title and icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('space-invaders.png')
+pygame.display.set_icon(icon)
+
+# Player
+playerimg = pygame.image.load('player64by64.png')
+player_width = playerimg.get_width()
+playerX = 370
+playerY = 480
+playerX_change = 0
+
+# enemy
+enemyimg = pygame.image.load('alien type 1.jpg')
+enemyX = random.randint(0,600)
+enemyY = random.randint(50,150)
+enemyX_change = 0.3
+enemyY_change = 40
+
+def player(x, y):
+    screen.blit(playerimg, (x, y))
+
+def enemy (x, y):
+    screen.blit(enemyimg, (x, y))
+
+# Game loop
+running = True
+while running:
+
+    screen.fill((0,0,0))   # clear screen
+
+    #background image
+    screen.blit(background, (0,0))
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            running = False
+
+        # key pressed(speed control)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.25
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.25
+
+        # key released
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+    # player movement
+    playerX += playerX_change
+
+    # enforce boundaries
+    playerX = max(0, min(playerX, 800 - playerimg.get_width()))
+
+    #enemy movement
+    enemyX += enemyX_change
+
+    if enemyX <= 0:
+        enemyX_change = 0.2
+        enemyY += enemyY_change
+
+    elif enemyX >= 736:
+        enemyX_change = -0.2
+        enemyY += enemyY_change
+
+
+
+    # draw player
+    player(playerX, playerY)
+    enemy(enemyX,enemyY)
+    pygame.display.update()
+
+
+
+
